6b9feef50924d88d3d788f63ef00b7186cbb52bf
Sherif Emad <142592673+sherifemad10@users.noreply.github.com>
2025-02-14 22:35:31 +0200

    Update page.js

--- src/app/video/page.js
+++ src/app/video/page.js
@@ -1,63 +1,30 @@
-// "use client"
-// import { ThemeContext } from '@/component/context/context';
-// import './video.modules.scss'
-// import { IoClose } from "react-icons/io5";
-// import { useContext } from 'react';
-// import Link from 'next/link';
-
-
-// const Video = () => {
-//     const {videoHandler} = useContext(ThemeContext)
-  
-//   return (
-//     <div className='Video'>
-
-// <div className='Video-container'>
-//   <Link href='/'>
-//      <IoClose onClick={videoHandler} className='close-icon'/>
-//      </Link>
-//       <video autoPlay loop={true}>
-//         <source src='/Nutrition-Clinic.mp4' type='video/mp4' />
-//       </video>
-//       </div>
-
-//     </div>
-//   )
-// }
-
-// export default Video
-
-"use client";
-import { ThemeContext } from "@/component/context/context";
-import "./video.modules.scss";
+"use client"
+import { ThemeContext } from '@/component/context/context';
+import './video.modules.scss'
 import { IoClose } from "react-icons/io5";
-import { useContext, useEffect, useState } from "react";
-import Link from "next/link";
+import { useContext } from 'react';
+import Link from 'next/link';
+
 
 const Video = () => {
-  const { videoHandler } = useContext(ThemeContext);
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    // Ensure the video loads as soon as the component mounts
-    setIsLoaded(true);
-  }, []);
-
-  if (!isLoaded) return null; // Prevents hydration issues in Next.js
-
+    const {videoHandler} = useContext(ThemeContext)
+  
   return (
-    <div className="Video">
-      <div className="Video-container">
-        <Link href="/">
-          <IoClose onClick={videoHandler} className="close-icon" />
-        </Link>
-        <video autoPlay loop muted playsInline>
-          <source src="/Nutrition-Clinic.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-    </div>
-  );
-};
+    <div className='Video'>
 
-export default Video;
+<div className='Video-container'>
+  <Link href='/'>
+     <IoClose onClick={videoHandler} className='close-icon'/>
+     </Link>
+      <video autoPlay loop={true}>
+        <source src='/Nutrition-Clinic.mp4' type='video/mp4' />
+      </video>
+      </div>
+
+    </div>
+  )
+}
+
+export default Video
+
+

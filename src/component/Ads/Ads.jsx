f314f96c35ebc65a80620320f1fafdece76ab2e7
Sherif Emad <142592673+sherifemad10@users.noreply.github.com>
2025-02-20 18:33:20 +0200

    Update Ads.jsx

--- src/component/Ads/Ads.jsx
+++ src/component/Ads/Ads.jsx
@@ -4,7 +4,7 @@ const Ads = () => {
   return (
     <>
 
-      <script type="text/javascript">
+      <script type="text/javascript" className='ads'>
 	atOptions = {
 		'key' : '951409e9961f7720e3e80e9c9db291e6',
 		'format' : 'iframe',

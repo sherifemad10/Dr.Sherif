421071f65a18c925ab655d3925b40d782fbfb367
Sherif Emad <142592673+sherifemad10@users.noreply.github.com>
2025-02-20 18:27:49 +0200

    Add files via upload

---
+++ src/component/Ads/Ads.jsx
@@ -0,0 +1,23 @@
+
+
+const Ads = () => {
+  return (
+    <>
+      <iframe
+  data-aa={2382361}
+  src="//ad.a-ads.com/2382361?size=728x90"
+  style={{
+    width: 728,
+    height: 90,
+    border: 0,
+    padding: 0,
+    overflow: "hidden",
+    backgroundColor: "transparent"
+  }}
+/>
+
+    </>
+  )
+}
+
+export default Ads

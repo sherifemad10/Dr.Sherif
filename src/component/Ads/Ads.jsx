207b19d38c954639fd786cf30bc9e7815a9b6308
Sherif Emad <142592673+sherifemad10@users.noreply.github.com>
2025-02-20 18:30:02 +0200

    Update Ads.jsx

--- src/component/Ads/Ads.jsx
+++ src/component/Ads/Ads.jsx
@@ -16,6 +16,17 @@ const Ads = () => {
   }}
 />
 
+      <script type="text/javascript">
+	atOptions = {
+		'key' : '951409e9961f7720e3e80e9c9db291e6',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//www.highperformanceformat.com/951409e9961f7720e3e80e9c9db291e6/invoke.js"></script>
+
     </>
   )
 }

583bf7e11eec35e6b06a8696d00afb1b4ae33a8e
Sherif Emad <142592673+sherifemad10@users.noreply.github.com>
2025-02-20 18:37:28 +0200

    Update Ads.jsx

--- src/component/Ads/Ads.jsx
+++ src/component/Ads/Ads.jsx
@@ -15,7 +15,7 @@ const Ads = () => {
 </script>
 <script type="text/javascript" src="//www.highperformanceformat.com/951409e9961f7720e3e80e9c9db291e6/invoke.js"></script>
 
-    <div/>
+    </div>
   )
 }
 

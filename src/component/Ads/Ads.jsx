12ea9fc0c2ba2b50e7184c1beb77d2b03be201b6
Sherif Emad <142592673+sherifemad10@users.noreply.github.com>
2025-04-30 21:33:42 +0300

    Update Ads.jsx

--- src/component/Ads/Ads.jsx
+++ src/component/Ads/Ads.jsx
@@ -1,75 +1,25 @@
-// import './ads.modules.scss';
-
-// const Ads = () => {
-//   return (
-//     <div className="ads">
-
-//       <div className="Ads-computer">
-//       <div className="Ads-content">
-// {/*         <h3>Ads</h3> */}
-//   <div>
-//     <script type="text/javascript">
-//         aclib.runBanner({
-//             zoneId: '9888586',
-//         });
-//     </script>
-// </div>
-
-
-//       </div>
-//       </div>
-
-//       {/* mobile ads */}
-//       <div className="Ads-mobile">
-//         <div className="Ads-content">
-//           <h3>Ads</h3>
-//         </div>
-//       </div>
-    
-//     </div>
-//   );
-// };
-// <script id="aclib" type="text/javascript" src="//acscdn.com/script/aclib.js"></script>
-
-// export default Ads;
-
-'use client'; // if you're using app directory and this is a client component
-
-import { useEffect } from 'react';
 import './ads.modules.scss';
 
 const Ads = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = '//acscdn.com/script/aclib.js';
-    script.id = 'aclib';
-    script.type = 'text/javascript';
-    script.async = true;
-
-    script.onload = () => {
-      if (window.aclib) {
-        window.aclib.runBanner({
-          zoneId: '9888586',
-        });
-      }
-    };
-
-    document.body.appendChild(script);
-
-    return () => {
-      const scriptEl = document.getElementById('aclib');
-      if (scriptEl) {
-        document.body.removeChild(scriptEl);
-      }
-    };
-  }, []);
-
   return (
     <div className="ads">
+
       <div className="Ads-computer">
-        <div className="Ads-content">
-          <div id="ad-container" />
-        </div>
+      <div className="Ads-content">
+        <h3>Ads</h3>
+<script type="text/javascript">
+	atOptions = {
+		'key' : '951409e9961f7720e3e80e9c9db291e6',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//www.highperformanceformat.com/951409e9961f7720e3e80e9c9db291e6/invoke.js"></script>
+
+
+      </div>
       </div>
 
       {/* mobile ads */}
@@ -78,9 +28,11 @@ const Ads = () => {
           <h3>Ads</h3>
         </div>
       </div>
+    
     </div>
   );
 };
+<script id="aclib" type="text/javascript" src="//acscdn.com/script/aclib.js"></script>
 
 export default Ads;
 

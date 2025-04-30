c11955d8a691b0066fe267d8c08656bae596df82
Sherif Emad <142592673+sherifemad10@users.noreply.github.com>
2025-04-30 21:18:16 +0300

    Update Ads.jsx

--- src/component/Ads/Ads.jsx
+++ src/component/Ads/Ads.jsx
@@ -2,22 +2,23 @@ import './ads.modules.scss';
 
 const Ads = () => {
   return (
-    <div className="ads-container">
-      <script type="text/javascript">
-        {`
-          atOptions = {
-            'key': '951409e9961f7720e3e80e9c9db291e6',
-            'format' : 'iframe',
-            'height' : 90,
-            'width' : 728,
-            'params' : {}
-          };
-        `}
-      </script>
-      <script
-        type="text/javascript"
-        src="//www.highperformanceformat.com/951409e9961f7720e3e80e9c9db291e6/invoke.js"
-      ></script>
+    <div className="ads">
+
+      <div className="Ads-computer">
+      <div className="Ads-content">
+        <h3>Ads</h3>
+  
+
+      </div>
+      </div>
+
+      {/* mobile ads */}
+      <div className="Ads-mobile">
+        <div className="Ads-content">
+          <h3>Ads</h3>
+        </div>
+      </div>
+    
     </div>
   );
 };

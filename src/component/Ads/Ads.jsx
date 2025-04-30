0092ec9bf58e83dc0272b3ff1d13bbcec684e577
Sherif Emad <142592673+sherifemad10@users.noreply.github.com>
2025-04-30 21:37:20 +0300

    Update Ads.jsx

--- src/component/Ads/Ads.jsx
+++ src/component/Ads/Ads.jsx
@@ -1,38 +1,55 @@
+'use client'; // Add this if using the app directory
+
+import { useEffect } from 'react';
 import './ads.modules.scss';
 
 const Ads = () => {
+  useEffect(() => {
+    // Define global atOptions before script is loaded
+    window.atOptions = {
+      key: '951409e9961f7720e3e80e9c9db291e6',
+      format: 'iframe',
+      height: 90,
+      width: 728,
+      params: {},
+    };
+
+    // Create and append the ad script
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//www.highperformanceformat.com/951409e9961f7720e3e80e9c9db291e6/invoke.js';
+    script.async = true;
+
+    const container = document.getElementById('desktop-ad-slot');
+    if (container) {
+      container.appendChild(script);
+    }
+
+    // Optional cleanup
+    return () => {
+      if (container && script.parentNode === container) {
+        container.removeChild(script);
+      }
+    };
+  }, []);
+
   return (
     <div className="ads">
-
+      {/* Desktop ads */}
       <div className="Ads-computer">
-      <div className="Ads-content">
-        <h3>Ads</h3>
-<script type="text/javascript">
-	atOptions = {
-		'key' : '951409e9961f7720e3e80e9c9db291e6',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
-</script>
-<script type="text/javascript" src="//www.highperformanceformat.com/951409e9961f7720e3e80e9c9db291e6/invoke.js"></script>
-
-
-      </div>
+        <div className="Ads-content">
+          <div id="desktop-ad-slot" />
+        </div>
       </div>
 
-      {/* mobile ads */}
+      {/* Mobile ads */}
       <div className="Ads-mobile">
         <div className="Ads-content">
           <h3>Ads</h3>
         </div>
       </div>
-    
     </div>
   );
 };
-<script id="aclib" type="text/javascript" src="//acscdn.com/script/aclib.js"></script>
 
 export default Ads;
-

aa41ae567fa9a68b835450b0f78317988254b949
Sherif Emad <142592673+sherifemad10@users.noreply.github.com>
2025-04-30 21:23:44 +0300

    Update Ads.jsx

--- src/component/Ads/Ads.jsx
+++ src/component/Ads/Ads.jsx
@@ -1,15 +1,75 @@
+// import './ads.modules.scss';
+
+// const Ads = () => {
+//   return (
+//     <div className="ads">
+
+//       <div className="Ads-computer">
+//       <div className="Ads-content">
+// {/*         <h3>Ads</h3> */}
+//   <div>
+//     <script type="text/javascript">
+//         aclib.runBanner({
+//             zoneId: '9888586',
+//         });
+//     </script>
+// </div>
+
+
+//       </div>
+//       </div>
+
+//       {/* mobile ads */}
+//       <div className="Ads-mobile">
+//         <div className="Ads-content">
+//           <h3>Ads</h3>
+//         </div>
+//       </div>
+    
+//     </div>
+//   );
+// };
+// <script id="aclib" type="text/javascript" src="//acscdn.com/script/aclib.js"></script>
+
+// export default Ads;
+
+'use client'; // if you're using app directory and this is a client component
+
+import { useEffect } from 'react';
 import './ads.modules.scss';
 
 const Ads = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = '//acscdn.com/script/aclib.js';
+    script.id = 'aclib';
+    script.type = 'text/javascript';
+    script.async = true;
+
+    script.onload = () => {
+      if (window.aclib) {
+        window.aclib.runBanner({
+          zoneId: '9888586',
+        });
+      }
+    };
+
+    document.body.appendChild(script);
+
+    return () => {
+      const scriptEl = document.getElementById('aclib');
+      if (scriptEl) {
+        document.body.removeChild(scriptEl);
+      }
+    };
+  }, []);
+
   return (
     <div className="ads">
-
       <div className="Ads-computer">
-      <div className="Ads-content">
-        <h3>Ads</h3>
-  
-
-      </div>
+        <div className="Ads-content">
+          <div id="ad-container" />
+        </div>
       </div>
 
       {/* mobile ads */}
@@ -18,9 +78,9 @@ const Ads = () => {
           <h3>Ads</h3>
         </div>
       </div>
-    
     </div>
   );
 };
 
 export default Ads;
+
